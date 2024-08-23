cb326ec32916017b9e3840b54f81b549e8561e6a
Rafi Siddique <rafisiddique67@gmail.com>
2024-08-23 07:30:00 +0600

    header section completed

--- font&colors.js
+++ font&colors.js
@@ -3,6 +3,12 @@ tailwind.config = {
         extend: {
             colors: {
                 clifford: '#da373d',
+                'bg-primary': '#ABEF5F',
+                'bg-primary-light': 'rgba(171, 239, 95, 0.5)',
+                'text-primary': '#212121',
+                'text-secondary': '#737373',
+                'background-black': '#1C1C1C',
+                'text-hover-black': '#494949',
             },
             fontFamily: {
                 'manrope': ['Manrope', 'sans-serif']
